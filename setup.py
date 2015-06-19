4f0555345574d1aea8d829807526323810995c6b
Danielle Jenkins <djenkins@splunk.com>
2015-06-19 16:09:10 -0700

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,11 @@
+"""This file defines how dump2json is installed in a python environment.
+"""
+import setuptools
+
+setuptools.setup(
+    name='pytest-dump2json',
+    version='0.1.0',
+    py_modules=['dump2json'],
+    install_requires=['pytest'],
+    test_suite='test',
+)
