eb216f4ac3df18a22aca530a6b7f22bed6ee216a
Danielle Jenkins <djenkins@splunk.com>
2015-06-28 22:56:54 -0700

    Improve setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ import setuptools
 setuptools.setup(
     name='pytest-dump2json',
     version='0.1.0',
+    description = 'A pytest plugin for dumping test results to json.',
     py_modules=['dump2json'],
     install_requires=['pytest'],
     test_suite='test',
@@ -13,4 +14,9 @@ setuptools.setup(
             'pytest-dump2json = dump2json',
         ]
     },
+    author = 'Danielle Jenkins',
+    author_email = 'jenkinda@uw.edu',
+    keywords = ['testing', 'pytest', 'json'],
+    url = 'https://github.com/d6e/pytest-dump2json',
+    download_url = 'https://github.com/d6e/pytest-dump2json',
 )
